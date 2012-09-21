1565fd7f9f438aba53bed882989345c2ed268e82
David Paschich <dpassage@flophouse.com>
2012-09-21 14:46:58 -0700

    Initial clean "rails new" setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfairelection::Application.config.secret_token = 'e60717dfd1a941c4bca9c65a24db9954bbec5cffda89d4bc8fd8d4d7c707585f0b84e94e8205919e592ba17c6ada6d9d1354c3b845524e89dadb847685d9f590'
