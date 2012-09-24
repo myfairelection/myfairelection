23097485a89bc5ae4247e0d87cdd8ff70895a8a2
David Paschich <dpassage@flophouse.com>
2012-09-23 21:31:24 -0700

    add the apache config

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,11 +21,16 @@ set :deploy_to, "/home/#{user}/apps/#{application}"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  task :setup_config, roles: :app do
+    run "ln -nfs #{current_path}/config/apache.conf /etc/apache2/sites-available/#{application}"
+  end
+  after "deploy:setup", "deploy:setup_config"
+end
