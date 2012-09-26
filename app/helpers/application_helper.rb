c3bdae33b897e2ff09e1e3933decde04c9a2b7a0
David Paschich <dpassage@flophouse.com>
2012-09-25 22:49:09 -0700

    Add the signing section at the bottom of the front page; remove the sign up link from the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # magic methods so devise works
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
