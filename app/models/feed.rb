22244b4debbc98d9b912370a69d522ef50ab823d
David Paschich <dpassage@flophouse.com>
2012-10-06 16:28:02 -0700

    Started on load method for feed object

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,10 +1,21 @@
-require 'uri'
+require 'open-uri'
 
 class Feed < ActiveRecord::Base
   attr_accessible :url, :vip_id
   validates_presence_of :url
+  validates_uniqueness_of :url
 
   def url_basename
-    URI(url).path.rpartition("/")[2]
+    url.rpartition("/")[2]
+  end
+
+  def load
+    feed_file = open(url)
+    feed_xml = Nokogiri::XML(feed_file)
+
+    self.version = feed_xml.xpath("//vip_object/@schemaVersion").to_s
+    self.vip_id = feed_xml.xpath("//vip_id/text()").to_s
+    self.loaded = true
+    self.save
   end
 end
--- spec/models/feed_spec.rb
+++ spec/models/feed_spec.rb
@@ -12,7 +12,11 @@ describe Feed do
     @feed.url = nil
     @feed.should_not be_valid
   end
-  it "verifies uniqueness of url" 
+  it "verifies uniqueness of url" do
+    @feed.save
+    newfeed = Feed.new({:url => "https://example.com/test.txt"})
+    newfeed.should_not be_valid
+  end
   it "is valid without a vip_id" do
     @feed.vip_id = nil
     @feed.should be_valid
@@ -30,10 +34,34 @@ describe Feed do
     end
   end
   describe '#load' do
-    it "retrieves the specified url"
-    it "populates the version field"
-    it "populates the vip_id field"
-    it "updates the last loaded date"
-    it "creates 3 PollingLocation objects"
+    context "with a network url" do
+      before(:each) do
+        @feed = Feed.new({:url => "https://raw.github.com/votinginfoproject/vip-specification/master/sample_feed_for_v4.0.xml"})
+      end
+      it "retrieves the specified url" 
+    end
+    context "with a file url" do
+      before(:each) do
+        @feed = Feed.new({:url => "spec/test_feeds/sample_feed_for_v4.0.xml"})
+      end
+      it "populates the version field" do
+        @feed.load
+        @feed.version.should eq("4.0")
+      end
+      it "populates the vip_id field" do
+        @feed.load
+        @feed.vip_id.should eq("39")
+      end
+      it "marks the feed as loaded" do
+        @feed.load
+        @feed.should be_loaded
+      end
+      it "creates 3 PollingLocation objects" do
+        pending 'creation of the PollingLocation class'
+        expect {
+          @feed.load
+        }.to_change(PollingLocation.count).by(3)
+      end
+    end
   end
 end
