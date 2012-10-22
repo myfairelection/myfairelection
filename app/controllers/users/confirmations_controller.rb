bcf9507c28722ca75dedc808aa33826a7372404f
David Paschich <dpassage@flophouse.com>
2012-10-22 13:13:49 -0700

    Actually, setting the flash isn't necessary, it's already set.

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -4,7 +4,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   def after_resending_confirmation_instructions_path_for(resource_name)
     if signed_in?
-      set_flash_message(:notice, :send_instructions)
       root_path
     else
       new_session_path(resource_name)
