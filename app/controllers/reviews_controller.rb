a0b358f9fbfcf41829b3c5d57e637dab527c6f74
David Paschich <dpassage@flophouse.com>
2012-10-28 15:19:00 -0700

    feedback for user when creating review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,15 +6,11 @@ class ReviewsController < ApplicationController
     ip_address = request.env['HTTP_X-REAL-IP'] 
     ip_address ||= request.env['REMOTE_ADDR']
     Review.create!(params[:review].merge({user: user, polling_location: polling_location, ip_address: ip_address}))
+    flash[:notice] = "Thank you for your review!"
     redirect_to polling_location_path(polling_location)
   end
   def new
     polling_location = PollingLocation.find(params[:polling_location_id])
     @review = Review.new(polling_location: polling_location, user: current_user)
   end
-  # Workflow: 2 buttons on voter_info page per location
-  # One takes you to location page
-  # Other takes you to page to create review of that location
-  # also button on location page to go to create review page
-  # submission takes you to location page
 end
