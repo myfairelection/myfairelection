be3bba9267d4eeb64b018de5133c2c0d4752237b
David Paschich <dpassage@flophouse.com>
2012-10-28 15:07:28 -0700

    You can't post a review if you're not logged in. Changes links and enforces in controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_filter :authenticate_user!
   def create
     polling_location = PollingLocation.find(params[:polling_location_id])
     user = current_user
