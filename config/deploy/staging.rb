388f5bcccaa9572aa36a5e964481559c2f6ca887
David Paschich <dpassage@flophouse.com>
2012-10-29 19:01:58 -0700

    Use the master branch for staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-set :branch, 'staging'
+set :branch, 'master'
 role :web, "198.101.255.80"# Your HTTP server, Apache/etc
 role :app, "198.101.255.80" # This may be the same as your `Web` server
 role :db,  "198.101.255.80", :primary => true # This is where Rails migrations will run
