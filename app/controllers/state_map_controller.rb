6120137e8fc611fd87525e8037286b12e54239f2
David Paschich <dpassage@flophouse.com>
2012-11-03 12:25:10 -0700

    Now responds with json!

--- app/controllers/state_map_controller.rb
+++ app/controllers/state_map_controller.rb
@@ -4,5 +4,9 @@ class StateMapController < ApplicationController
 
   def states
     @states = StateMapData.all
+    respond_to do |format|
+      format.html { render :action => 'states' }
+      format.js { render :json => @states.to_json }
+    end
   end
 end
