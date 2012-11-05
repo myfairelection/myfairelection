5c7001c7871abb8c2a8bffe303cc8593a7c7e3bd
David Paschich <dpassage@flophouse.com>
2012-11-05 11:08:04 -0800

    fix admin server IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, 'production'
-role :web, "168.78.0.77", "198.61.213.70", "198.101.255.157", "98.129.169.198", "98.129.169.168", "98.129.169.60" # Your HTTP server, Apache/etc
-role :app, "168.78.0.77", "198.61.213.70", "198.101.255.157", "98.129.169.198", "98.129.169.168", "98.129.169.60" # This may be the same as your `Web` server
+role :web, "166.78.0.77", "198.61.213.70", "198.101.255.157", "98.129.169.198", "98.129.169.168", "98.129.169.60" # Your HTTP server, Apache/etc
+role :app, "166.78.0.77", "198.61.213.70", "198.101.255.157", "98.129.169.198", "98.129.169.168", "98.129.169.60" # This may be the same as your `Web` server
 role :db,  "166.78.0.77", :primary => true # This is where Rails migrations will run
