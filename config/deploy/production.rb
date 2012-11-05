801026431ea7bd700b446ec642d0be9f42d3301e
David Paschich <dpassage@flophouse.com>
2012-11-05 11:04:16 -0800

    Firing up more production servers.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, 'production'
-role :web, "198.61.213.70", "198.101.255.157" # Your HTTP server, Apache/etc
-role :app, "198.61.213.70", "198.101.255.157" # This may be the same as your `Web` server
-role :db,  "198.61.213.70", :primary => true # This is where Rails migrations will run
+role :web, "168.78.0.77", "198.61.213.70", "198.101.255.157", "98.129.169.198", "98.129.169.168", "98.129.169.60" # Your HTTP server, Apache/etc
+role :app, "168.78.0.77", "198.61.213.70", "198.101.255.157", "98.129.169.198", "98.129.169.168", "98.129.169.60" # This may be the same as your `Web` server
+role :db,  "166.78.0.77", :primary => true # This is where Rails migrations will run
