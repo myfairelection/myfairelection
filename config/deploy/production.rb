6db2ea49808a090d75cc8acac0f166b20ad133af
David Paschich <dpassage@flophouse.com>
2013-01-02 18:36:40 -0800

    Only one production server at the moment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :branch, 'production'
-role :web, "166.78.0.77", "198.61.213.70" # Your HTTP server, Apache/etc
-role :app, "166.78.0.77", "198.61.213.70" # This may be the same as your `Web` server
-role :db,  "166.78.0.77", :primary => true # This is where Rails migrations will run
+role :web, "198.61.213.70" # Your HTTP server, Apache/etc
+role :app, "198.61.213.70" # This may be the same as your `Web` server
+role :db,  "198.61.213.70", :primary => true # This is where Rails migrations will run
