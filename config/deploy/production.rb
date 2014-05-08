f967efc62e2d9151a30cae9eb4ab1f7210489b6d
David Paschich <dpassage@balveda.com>
2014-05-08 09:18:41 -0700

    Remove Capistrano and other artifacts of our deployment on Rackspace bare servers.

--- config/deploy/production.rb
+++
@@ -1,4 +0,0 @@
-set :branch, 'production'
-role :web, "198.61.213.70" # Your HTTP server, Apache/etc
-role :app, "198.61.213.70" # This may be the same as your `Web` server
-role :db,  "198.61.213.70", :primary => true # This is where Rails migrations will run
