3774c496cb0ba84ddc8553e7f4d1d6c4d0ab28ef
David Paschich <dpassage@balveda.com>
2014-05-22 13:20:31 -0700

    Fix ParenthesesAsGroupedExpression introduced with last fix

--- features/step_definitions/google_api_steps.rb
+++ features/step_definitions/google_api_steps.rb
@@ -1,3 +1,3 @@
-Given (/^the Google API is stubbed to return "([^"]*)"$/) do |fixture_name|
+Given(/^the Google API is stubbed to return "([^"]*)"$/) do |fixture_name|
   RestClient.stub(:post).and_return(File.open("spec/fixtures/voter_info_responses/#{fixture_name}").read)
 end
--- features/step_definitions/mfe_steps.rb
+++ features/step_definitions/mfe_steps.rb
@@ -1,16 +1,16 @@
-Given (/^I am a registered user$/) do
+Given(/^I am a registered user$/) do
   @user = FactoryGirl.create(:user)
 end
 
-Given (/^I am a registered admin user$/) do
+Given(/^I am a registered admin user$/) do
   @user = FactoryGirl.create(:admin)
 end
 
-Given (/^I am a registered user with email "([^"]*)" and password "([^"]*)"$/) do |email, password|
+Given(/^I am a registered user with email "([^"]*)" and password "([^"]*)"$/) do |email, password|
   @user = FactoryGirl.create(:user, email: email, password: password)
 end
 
-Given (/^I am a registered user with the address "([^\"]*)"$/) do |arg1|
+Given(/^I am a registered user with the address "([^\"]*)"$/) do |arg1|
   step 'I am a registered user'
   @user.address = Address.new('line1' => '1263 Pacific Avenue',
                               'city' =>'Kansas City',
@@ -19,18 +19,18 @@ Given (/^I am a registered user with the address "([^\"]*)"$/) do |arg1|
   @user.save
 end
 
-Given (/^I am a confirmed user with email "([^"]*)" and password "([^"]*)"$/) do |email, password|
+Given(/^I am a confirmed user with email "([^"]*)" and password "([^"]*)"$/) do |email, password|
   step "I am a registered user with email \"#{email}\" and password \"#{password}\""
   @user.confirmed_at = Time.now
   @user.save
 end
 
 
-Given (/^I am not logged in$/) do
+Given(/^I am not logged in$/) do
   # nothing to do here
 end
 
-Given (/^I log in$/) do
+Given(/^I log in$/) do
   visit '/'
   step "I fill in \"user_email\" in the form \"signin\" with \"#{@user.email}\""
   step "I fill in \"user_password\" in the form \"signin\" with \"#{@user.password}\""
@@ -38,63 +38,63 @@ Given (/^I log in$/) do
 end
 
 
-Given (/^I am on the home page$/) do
+Given(/^I am on the home page$/) do
   visit '/'
 end
 
-Given (/^there is a feed with the url "([^"]*)"$/) do |url|
+Given(/^there is a feed with the url "([^"]*)"$/) do |url|
   Feed.create!(url: url)
 end
 
-Given (/^there is a polling location$/) do
+Given(/^there is a polling location$/) do
   @polling_location = FactoryGirl.create(:polling_location)
 end
 
-Given (/^the site is in shutoff mode$/) do
+Given(/^the site is in shutoff mode$/) do
   Settings['shutoff'] = true
 end
 
-When (/^I am on the "([^"]*)" page$/) do |path|
+When(/^I am on the "([^"]*)" page$/) do |path|
   visit path
 end
 
-When (/^I visit the "([^"]*)" page$/) do |path|
+When(/^I visit the "([^"]*)" page$/) do |path|
   visit path
 end
 
-When (/^I visit the polling location page$/) do
+When(/^I visit the polling location page$/) do
   visit "/polling_locations/#{@polling_location.id}"
 end
 
-When (/^I fill in "([^"]*)" in the form "([^"]*)" with "([^"]*)"$/) do |field, form, value|
+When(/^I fill in "([^"]*)" in the form "([^"]*)" with "([^"]*)"$/) do |field, form, value|
   within("form\##{form}") do
     fill_in(field, with: value)
   end
 end
 
-When (/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, value|
   fill_in(field, with: value)
 end
 
-When (/^I click the "([^"]*)" button$/) do |button|
+When(/^I click the "([^"]*)" button$/) do |button|
   click_button(button)
 end
 
-When (/^I click the "([^"]*)" button in the form "([^"]*)"$/) do |button, form|
+When(/^I click the "([^"]*)" button in the form "([^"]*)"$/) do |button, form|
   within("form\##{form}") do
     click_button(button)
   end
 end
 
-When (/^I follow "([^"]*)"$/) do |link|
+When(/^I follow "([^"]*)"$/) do |link|
   click_link(link)
 end
 
-When (/^I follow the first "([^"]*)" link$/) do |link|
+When(/^I follow the first "([^"]*)" link$/) do |link|
   first(:link, link).click
 end
 
-When (/^I register as a new user$/) do
+When(/^I register as a new user$/) do
   step 'I visit the "/users/sign_up" page'
   step 'I fill in "user_email" in the form "new_user" with "voter@example.org"'
   step 'I fill in "user_password" in the form "new_user" with "voter123"'
@@ -102,31 +102,31 @@ When (/^I register as a new user$/) do
   step 'I click the "Register" button in the form "new_user"'
 end
 
-When (/^I click on the link in the email$/) do
+When(/^I click on the link in the email$/) do
   pending # express the regexp above with the code you wish you had
 end
 
-When (/^I run "([^"]*)"$/) do |command|
+When(/^I run "([^"]*)"$/) do |command|
   system command
 end
 
-When (/^I save and open the page$/) do
+When(/^I save and open the page$/) do
   puts current_path
   save_and_open_page
 end
 
-Then (/^I should see "([^"]*)"$/) do |content|
+Then(/^I should see "([^"]*)"$/) do |content|
   expect(page).to have_content(content)
 end
 
-Then (/^I should not see "([^"]*)"$/) do |content|
+Then(/^I should not see "([^"]*)"$/) do |content|
   expect(page).to_not have_content(content)
 end
 
-Then (/^I should see a button named "([^"]*)"$/) do |button|
+Then(/^I should see a button named "([^"]*)"$/) do |button|
   expect(page).to have_button(button)
 end
 
-Then (/^there should be "([^"]*)" "([^"]*)" objects in the database$/) do |n, klass|
+Then(/^there should be "([^"]*)" "([^"]*)" objects in the database$/) do |n, klass|
   Kernel.const_get(klass).count.should == n.to_i
 end
