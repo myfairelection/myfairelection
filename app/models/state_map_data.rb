8cbebed26f7b6c47387bf17c133aad3d34cf7d71
David Paschich <dpassage@balveda.com>
2014-05-22 13:37:07 -0700

    Fix WordArray

--- app/models/state_map_data.rb
+++ app/models/state_map_data.rb
@@ -1,16 +1,10 @@
 class StateMapData < ActiveRecord::Base
   attr_accessible :name, :rating, :wait_time, :n
 
-  STATES = ['AL', 'AK', 'AZ', 'AR', 'CA',
-            'CO', 'CT', 'DE', 'DC', 'FL',
-            'GA', 'HI', 'ID', 'IL', 'IN',
-            'IA', 'KS', 'KY', 'LA', 'ME',
-            'MD', 'MA', 'MI', 'MN', 'MS',
-            'MO', 'MT', 'NE', 'NV', 'NH',
-            'NJ', 'NM', 'NY', 'NC', 'ND',
-            'OH', 'OK', 'OR', 'PA', 'RI',
-            'SC', 'SD', 'TN', 'TX', 'UT',
-            'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+  STATES = %w(AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN
+              IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH
+              NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT
+              VT VA WA WV WI WY)
 
   def self.update
     transaction do
