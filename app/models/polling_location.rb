7461ff3852a14a4f5b3a021762e06c6be7bf6acb
David Paschich <dpassage@balveda.com>
2014-05-22 13:40:15 -0700

    Fix HashSyntax

--- app/models/polling_location.rb
+++ app/models/polling_location.rb
@@ -2,8 +2,8 @@ class PollingLocation < ActiveRecord::Base
   attr_accessible :early_vote, :line1, :line2, :line3, :city, :state, :zip,
                   :name, :location_name, :county, :latitude, :longitude,
                   :properties
-  validates :state, :format => { :with => /\A[A-Z][A-Z]\z/ },
-                    :allow_nil => true
+  validates :state, format: { with: /\A[A-Z][A-Z]\z/ },
+                    allow_nil: true
   validate :at_least_one_address_field_must_be_present
   validate :if_description_set_only_state_can_be_present
   serialize :properties, JSON
